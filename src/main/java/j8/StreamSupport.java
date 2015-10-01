7dbf366d7f7cc460e7c0cc011f37729bc8704e69
Brad Fritz <brad@fewerhassles.com>
2015-10-01 14:25:28 -0400

    support for pulling inputs from environment variable
    Add `StreamSupport.findInputs()` utility method that parses the
    `FILES` environment variable and returns an iterable over the
    files contents.
    
    `FILES` is assumed to be a whitespace delimited list of filenames
    (which assumes our filenames don't contain spaces).

---
+++ src/main/java/j8/StreamSupport.java
@@ -0,0 +1,23 @@
+package org.indyscala.streams.support;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.GZIPInputStream;
+
+public class StreamSupport {
+
+    private static final String ENV_FILE_LIST = "FILES";
+
+    public static Iterable<InputStream> findInputs() throws IOException {
+        List<InputStream> files = new ArrayList<>();
+        for (String file : extractFilenames(System.getenv(ENV_FILE_LIST))) {
+            files.add(
+                    new GZIPInputStream(new FileInputStream(file)));
+        }
+        return files;
+    }
+
+    private static String[] extractFilenames(String paths) {
+        return paths.split("\\s+");
+    }
+}
