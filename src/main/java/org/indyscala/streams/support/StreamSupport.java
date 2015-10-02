5b900e3a529cf6934d400dc87a59062758f5e729
Brad Fritz <brad@fewerhassles.com>
2015-10-01 22:48:05 -0400

    log names of input files

--- src/main/java/org/indyscala/streams/support/StreamSupport.java
+++ src/main/java/org/indyscala/streams/support/StreamSupport.java
@@ -6,13 +6,19 @@ import java.util.*;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StreamSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(StreamSupport.class);
 
     private static final String ENV_FILE_LIST = "FILES";
 
     public static Iterable<InputStream> findInputs() throws IOException {
         List<InputStream> files = new ArrayList<>();
         for (String file : extractFilenames(System.getenv(ENV_FILE_LIST))) {
+            logger.debug("Adding {} to input list.", file);
             files.add(
                     file.endsWith(".gz")
                     ? new GZIPInputStream(new FileInputStream(file))
