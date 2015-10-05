ffc21474dfeed9d61753e00d9d6eaf2cb1cfb475
Brad Fritz <brad@fewerhassles.com>
2015-10-05 16:00:16 -0400

    add JSON parsing to scalaz-stream demo
    Assumes no parsing failures.

--- src/main/scala/org/indyscala/streams/sstream/Demo.scala
+++ src/main/scala/org/indyscala/streams/sstream/Demo.scala
@@ -2,6 +2,8 @@ package org.indyscala.streams.sstream
 
 import org.indyscala.streams.support.StreamSupport.findInputs
 
+import org.json4s.JsonAST.JValue
+
 import scala.collection.JavaConverters._
 import scala.io.Codec.UTF8
 
@@ -9,12 +11,12 @@ import scalaz.concurrent.Task
 import scalaz.stream._
 
 object Demo {
+  val NoLines: Process[Task,String] = Process.halt
 
   def countLines(): Result = {
-    val empty: Process[Task,String] = Process.halt
     val lines: Process[Task,String] = findInputs   // findInputs is Iterable[InputStream]
       .asScala
-      .foldLeft(empty)(_ ++ io.linesR(_)(UTF8))
+      .foldLeft(NoLines)(_ ++ io.linesR(_)(UTF8))
 
     val counter: Task[Option[Int]] = lines
       .map(_ => 1)
@@ -25,6 +27,31 @@ object Demo {
 
     Result(
       count=count.map(_.toLong))
+  }
+
+  def countJson(): Result = {
+    val lines: Process[Task,String] = findInputs
+      .asScala
+      .foldLeft(NoLines)(_ ++ io.linesR(_)(UTF8))
+
+    val counter = lines
+      .map(parseJson)
+      .map(_ => 1)
+      .sum
+      .runLast
+
+    val count = counter.run
+
+    Result(
+      count=count.map(_.toLong))
+  }
+
+  private def parseJson(json: String): JValue = {
+    import org.json4s._
+    import org.json4s.jackson.JsonMethods._
+    implicit val formats = DefaultFormats
+
+    parse(json)
   }
 
   case class Result(count: Option[Long] = None, parsed: Option[Long] = None, errors: Option[Long] = None)
--- src/test/scala/org/indyscala/streams/sstream/DemoSpec.scala
+++ src/test/scala/org/indyscala/streams/sstream/DemoSpec.scala
@@ -8,4 +8,9 @@ class DemoSpec extends UnitSpec("SSTREAMS") {
     val r = Demo.countLines()
     info(s"counted ${r.count.get} lines")
   }
+
+  "Parsing JSON (simple)" should "complete without error" in {
+    val r = Demo.countJson()
+    info(s"counted ${r.count.get} lines")
+  }
 }
